4d0124190e753f4978cb6385948385510e9c24a3
giocic2 <74670040+giocic2@users.noreply.github.com>
2021-11-24 15:10:52 +0100

    sampling freq 1MHz
    For testing purposes, this became the standard sampling frequency, so i can see all the spurius signals

--- _single-direction-analisys.py
+++ _single-direction-analisys.py
@@ -22,7 +22,7 @@ plt.grid(True)
 plt.show()
 
 FFT_FREQ_BINS = 2**20
-SAMPLING_FREQUENCY = 100e3 # According to "hrc-ps.py" script
+SAMPLING_FREQUENCY = 1e6 # According to "hrc-ps.py" script
 print('FFT resolution: ' + str(SAMPLING_FREQUENCY/FFT_FREQ_BINS) + ' Hz')
 
 # FFT computation
